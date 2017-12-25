ac2c76cc6a756e00384e5da70984aa049afbf29f
gaojiming <gaojiming@thczht.com>
2017-12-25 17:37:22 +0800

    util

--- raft.go
+++ raft.go
@@ -153,6 +153,8 @@ type raft struct {
 
 	votes map[uint64]bool
 
+	//TODO 
+	//消息缓存,发给谁的消息???发送给多个对端的消息，如何判断真正是发给那一个对端的
 	msgs []pb.Message
 
 	//leader id
@@ -262,6 +264,135 @@ func newRaft(c *Config) *raft {
 	return r
 }
 
+func (r *raft) hasLeader() bool {	return r.lead != None	}
+
+func(r *raft) softState() * SoftState {
+	return &SoftState{Lead:r.lead,RaftState:r.state}
+}
+
+func (r *raft) hardState() pb.HardState {
+	return pb.HardState {
+		Term: r.Term,
+		Vote: r.Vote,
+		Commit:	r.raftLog.committed,
+	}
+}
+
+func (r *raft) quorum() int {
+	return len(r.prs)/2+1
+}
+
+func (r *raft) nodes() []uint64 {
+	nodes := make([]uint64,0,len(r.prs)+len(r.learnerPrs))
+
+	for id := range  r.prs {
+		nodes = append(nodes,id)
+	}
+	for id := range r.learnerPrs {
+		nodes = append(nodes,id)
+	}
+
+	sort.Sort(uint64Slice(nodes))
+	return nodes
+}
+
+//保存到storage中然后发送到mailbox中
+func (r *raft) send(m pb.Message) {
+	m.From = r.id 
+	if m.Type == pb.MsgVote || m.Type == pb.MsgVoteResp || m.Type == pb.MsgPreVote || m.Type == pb.MsgPreVoteResp {
+		if m.Term == 0 {
+			//所有竞选信息需要在发送信息的时候设置term
+			//MsgVote:参加竞选的term编号，参加竞选时增加肯定不为0
+			//MsgVoteResp:是reply的term。若被选上，肯定也不为0
+			//MsgPreVote:将要参加竞选的term，因为要根据当前term推测下一个term用来竞选，所以不为0
+			//MsgPreVoteResp:若prevote被接受，none zero
+			panic(fmt.Sprintf("term should be set when send %s",m.Type))
+		}
+	}else {
+		//当发送正常信息的时候term不应该被设置,
+		if m.Term != 0 {
+			panic(fmt.Sprintf("term should be set when sending %s (was %d)",m.Type,m.Term))
+		}
+		//被设置为当前raft的term编号
+		if m.Type != pb.MsgProp && m.Type != pb.MsgReadIndex {
+			m.Term = r.Term
+		}
+	}
+	r.msgs = append(r.msgs,m)
+}
+
+func (r *raft) sendHeartbeat(to uint64,ctx []byte) {
+	、、m:=
+}
+
+func (r *raft) getProgress(id uint64 ) *Progress {
+	if pr,ok := r.prs[id]; ok {
+		return pr
+	}
+	return r.learnerPrs[id]
+}
+
+// 通过rpc将entries发送到指定的对端
+func (r *raft) sendAppend(to uint64) {
+	pr := r.getProgress(to)
+
+	if pr.IsPaused() {
+		return 
+	}
+
+	m := pb.Message{}
+	m.To = to
+	term,errt := r.raftLog.term(pr.Next -1)
+	ents,erre := r.raftLog.entries(pr.Next,r.maxMsgSize)
+
+	//若获取term和entries失败，发送snapshot给对端,并将对端progress设置为snapshot
+	if errt != nil || erre != nil {
+		if !pr.RecentActive {
+			//对端可能挂了
+			r.logger.Debugf("ignore sending snapshot to %x since it is not recently active ",to )
+			return 
+		}
+		m.Type = pb.MsgSnap
+		snapshot,err := r.raftLog.snapshot()
+		if err != nil {
+			if err == ErrSnapshotTemporarilyUnavailable {
+				r.logger.Debugf("%x failed to send snapshot to %x because snapshot is temporarily unavailable",r.id,to)
+				return 
+			}
+			panic(err)
+		}
+		if IsEmptySnap(snapshot) {
+			panic("need non-empty snapshot")
+		}
+		m.Snapshot = snapshot
+		sindex,sterm := snapshot.Metadata.Index,snapshot.Metadata.Term
+		r.logger.Debugf("%x [firstindex: %d, commit: %d] send snapshot[index: %d,term: %d] to %x [%s]",
+			r.id,r.raftLog.firstIndex(),r.raftLog.committed,sindex,sterm,to,pr)
+		pr.becomeSnapshot(sindex)
+		r.logger.Debugf("%x paused sending replication messages to %s [%s]",r.id,to,pr)
+	}else {
+		//正常发送消息。根据对端的progress状态决定.
+		//发送消息到raft的msg中，在progress中保存此消息的inflights
+		m.Type = pb.MsgApp
+		m.Index = pr.Next -1
+		m.LogTerm = term
+		m.Entries = ents
+		m.Commit = r.raftLog.committed
+		if n:= len(m.Entries); n != 0 {
+			switch pr.State {
+			case ProgressStateReplicate:
+				last := m.Entries[n-1].Index
+				pr.optimisticUpdate(last)
+				pr.ins.add(last)
+			case ProgressStateProbe:
+				pr.pause()
+			default:
+				r.logger.Panicf("%x is sending append in unhandles state %s",r.id,pr.State)
+			}
+		}
+	}
+	r.send(m)
+}
 
 type stepFunc func(r *raft, m pb.Message)
 
